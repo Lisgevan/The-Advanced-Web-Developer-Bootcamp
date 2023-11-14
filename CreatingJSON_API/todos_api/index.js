7e477b74b230015ab901d53fd28f4041d4187e14
Lisgevan <episimo@hotmail.com>
2023-11-14 21:01:44 +0200

    Single Page Todo List - section finished

--- CreatingJSON_API/todos_api/index.js
+++ CreatingJSON_API/todos_api/index.js
@@ -9,8 +9,10 @@ const todoRoutes = require("./routes/todos");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/views"));
+app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => res.send("HELLO FROM THE ROOT ROUTE"));
+app.get("/", (req, res) => res.sendFile("index.html"));
 
 app.use("/api/todos", todoRoutes);
 
